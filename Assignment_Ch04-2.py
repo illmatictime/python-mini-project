6524c07d7f7dd75c6b3a1c3149f4a0ed09a553b7
illmatictime <jerrylanderos@gmail.com>
2020-03-19 19:59:54 -0700

    updated

--- Assignment_Ch04-2.py
+++ Assignment_Ch04-2.py
@@ -2,37 +2,48 @@ aUser = input("Please enter the first integer: ")
 bUser = input("Please enter the second integer: ")
 cUser = input("Please enter the third integer: ")
 
-# Error Check
-
-
-def positiveIntegerCheck(value):
-    try:
-        value = int(value)
-    except Exception:
-        print("Please input aUser valid Integer!")
-        quit()
 
 # Compare Inputs
 
+inputSorted = ""
+
 
 def compareInput(aUser, bUser, cUser):
-    positiveIntegerCheck(aUser)
-    positiveIntegerCheck(bUser)
-    positiveIntegerCheck(cUser)
+    integerCheck(aUser)
+    integerCheck(bUser)
+    integerCheck(cUser)
     aUser = int(aUser)
     bUser = int(bUser)
     cUser = int(cUser)
 
     if aUser >= bUser and bUser >= cUser:
-        asorted = str(cUser)+" "+str(bUser)+" "+str(aUser)
+        inputSorted = str(cUser)+" "+str(bUser)+" "+str(aUser)
     elif aUser >= cUser and cUser >= bUser:
-        asorted = str(bUser)+" "+str(cUser)+" "+str(aUser)
+        inputSorted = str(bUser)+" "+str(cUser)+" "+str(aUser)
     elif bUser >= aUser and aUser >= cUser:
-        asorted = str(cUser)+" "+str(aUser)+" "+str(bUser)
+        inputSorted = str(cUser)+" "+str(aUser)+" "+str(bUser)
     elif bUser >= cUser and cUser >= aUser:
-        asorted = str(aUser)+" "+str(cUser)+" "+str(bUser)
+        inputSorted = str(aUser)+" "+str(cUser)+" "+str(bUser)
     elif cUser >= bUser and bUser >= aUser:
-        asorted = str(aUser)+" "+str(bUser)+" "+str(cUser)
+        inputSorted = str(aUser)+" "+str(bUser)+" "+str(cUser)
     elif cUser >= aUser and aUser >= bUser:
-        asorted = str(bUser)+" "+str(aUser)+" "+str(cUser)
-    return asorted
+        inputSorted = str(bUser)+" "+str(aUser)+" "+str(cUser)
+    return inputSorted
+
+# Error Check
+
+
+def integerCheck(value):
+    try:
+        value = int(value)
+    except Exception:
+        print("Please input a valid Integer!")
+        quit()
+
+
+def printResults(inputSorted):
+    print("Input three integer numbers in ascending order::\n" + inputSorted)
+
+
+printResults(inputSorted)
+compareInput(aUser, bUser, cUser)
