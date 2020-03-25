e49a172f2817a25dd69bacd834e367c113cb49b8
illmatictime <jerrylanderos@gmail.com>
2020-03-24 22:46:33 -0700

    new print lines

--- Assignment_Ch03-2.py
+++ Assignment_Ch03-2.py
@@ -11,27 +11,22 @@ except Exception:
     print("Not a valid input")
     quit()
 
+
 if a >= b and b >= c:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(c) + " " + str(b) + " " + str(a)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(c, b, a)
 elif a >= c and c >= b:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(b) + " " + str(c) + " " + str(a)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(b, c, a)
 elif b >= a and a >= c:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(c) + " " + str(a) + " " + str(b)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(c, a, b)
 elif b >= c and c >= a:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(a) + " " + str(c) + " " + str(b)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(a, c, b)
 elif c >= b and b >= a:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(a) + " " + str(b) + " " + str(c)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(a, b, c)
 elif c >= a and a >= b:
-    print(
-        "Input three integer numbers in ascending order:\n" + str(b) + " " + str(a) + " " + str(c)
-    )
+    print("Input three integer numbers in ascending order:")
+    print(b, a, c)
