dab8b88e6a930e2693c41cba4a4f18bb1c9b8fe3
illmatictime <jerrylanderos@gmail.com>
2020-04-02 00:33:51 -0700

    new file 6-1

---
+++ Ch06-1_JLande.py
@@ -0,0 +1,9 @@
+myInput = input("Enter a string with a colon and a number after it: ")
+
+findColon = myInput.find(":")
+
+# aNumber = slice(findColon+1, len(myInput))
+myInput = myInput[findColon+1:len(myInput)]
+
+myInput = float(myInput)+2.0
+print("Extracted a number from given string and added 2.0 to it:", myInput)
