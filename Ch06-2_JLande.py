21f92a4fda8b382b91ac0ca81172c3c4c90af5fc
illmatictime <jerrylanderos@gmail.com>
2020-04-02 19:02:51 -0700

    ch6-2 almost complete

--- Ch06-2_JLande.py
+++ Ch06-2_JLande.py
@@ -1,16 +1,46 @@
-def repeatProgram():
-    myString = input("Enter a string with two " + "!" + "surrounding of the string: ")
-    number1 = myString.find("!")
-    number2 = myString.rfind("!")
-    newString = myString[number1 + 1 : number2]
-    newString = newString[len(newString) :: -1]
+def stringFinder():
+    myString = input("Enter a string with two " + "!" +
+                     "surrounding of the string: ")
+    while True:
+        try:
+            number1 = myString.find("!")
+            number2 = myString.rfind("!")
+            newString = myString[number1 + 1: number2]
+            word = newString[len(newString):: -1]
+            break
+        except Exception:
+            print("Please enter a string with a word surrounded by !!:")
+    return word
 
-    index = len(newString)
-    newString1 = []
-    while index > 0:
-        newString1 += newString[index - 1]
-        index = index - 1
-        print(newString1)
+
+def stringPrinter(word):
+    for letter in word:
+        print(letter)
+    # index = 0
+    # while index < len(newString):
+    #     singleLetter = newString[index]
+    #     print(singleLetter)
+    #     index = index + 1
 
 
-repeatProgram()
+def repeat():
+    repeat = input("Do you want to try again? (y/n)")
+    if repeat.upper() == "Y":
+        print("")
+        main()
+    elif repeat.upper() == "N":
+        print("Thank You for playing.")
+        quit()
+    else:
+        print("Please enter y or n")
+        repeat()
+
+
+def main():
+    while True:
+        word = stringFinder()
+        stringPrinter(word)
+        repeat()
+
+
+main()
